ff6c642c62c1be9e9d57ae9bd95b22b58a0888b7
Flashman2000 <32598745+Flashman2000@users.noreply.github.com>
2020-10-04 15:49:58 -0400

    added 7049_D - 7040_D

--- sbatch.sh
+++ sbatch.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-sbatch 7057_D.slurm
-sbatch 7056_D.slurm
-sbatch 7055_D.slurm
-sbatch 7054_D.slurm
-sbatch 7053_D.slurm
-sbatch 7052_D.slurm
-sbatch 7051_D.slurm
-sbatch 7050_D.slurm
+sbatch 7049_D.slurm
+sbatch 7048_D.slurm
+sbatch 7047_D.slurm
+sbatch 7046_D.slurm
+sbatch 7045_D.slurm
+sbatch 7044_D.slurm
+sbatch 7043_D.slurm
+sbatch 7042_D.slurm
+sbatch 7041_D.slurm
+sbatch 7040_D.slurm
