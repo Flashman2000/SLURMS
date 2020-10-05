042049853f7c090ae0eb7e67b01151d5afd494e6
Flashman2000 <32598745+Flashman2000@users.noreply.github.com>
2020-10-05 16:47:45 -0400

    testing stuff

---
+++ sbatch_BQSR.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sbatch 7039_D.slurm
+sbatch 7038_D.slurm
+sbatch 7036_D.slurm
+sbatch 7035_D.slurm
+sbatch 7034_D.slurm
+sbatch 7033_D.slurm
+sbatch 7030_D.slurm
+sbatch 7029_D.slurm
+sbatch 7028_D.slurm
--- test.sj.sh
+++ test.sj.sh
@@ -2,4 +2,5 @@
 
 for d in /mnt/rds/txl80/LaframboiseLab/vst14/merged_bams/
 do
-	sbatch --export=dir=$d --file-name=
+	echo $d
+done
