54ec6702d7759e7ee78528a70d08fe7b4ef839aa
Thorat;Vedant <vedant.kt@gmail.com>
2020-10-12 12:37:13 -0400

    created gvcf samplemap

--- tablegen.sh
+++ tablegen.sh
@@ -3,6 +3,6 @@
 for d in /mnt/rds/txl80/LaframboiseLab/vst14/AR_BAM/*.g.vcf.gz
 do
 	File="$(basename -- $d)"
-  	FN=${File%.*}
-  	echo $FN$'\t'$FN.g.vcf.gz
+	FN=${File%.g.*}
+  echo $FN$'\t'$FN.g.vcf.gz >> /mnt/rds/txl80/LaframboiseLab/vst14/AR_BAM/cohort.sample_map
 done
