c336b0b83f0fe869a043d6a72a553d47707b9f43
Thorat;Vedant <vedant.kt@gmail.com>
2020-10-19 19:33:11 -0400

    more stuff

--- testsh.sh
+++ testsh.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-for d in /mnt/rds/txl80/LaframboiseLab/vst14/AR_BAM/*.bam
+for d in /mnt/rds/txl80/LaframboiseLab/vst14/AR_BAM/*.g.vcf.gz
 do
   cd /mnt/rds/txl80/LaframboiseLab/vst14/AR_BAM/
   File="$(basename -- $d)"
   FN=${File%.*}
-  if [ -f "$FN.g.vcf.gz" ]; then
-    echo "$FN processed/processing"
-  else
-    echo "$FN not processed"
-  fi
+  echo "--in-gvcf $File" >> /mnt/rds/txl80/LaframboiseLab/vst14/pbtest_DB.sh
+  #if [ -f "$FN.g.vcf.gz" ]; then
+   # echo "$FN processed/processing"
+  #else
+   # echo "$FN not processed"
+  #fi
 done
