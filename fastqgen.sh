b43f5f40b8ff15e23d7d75575d9a6f5e911deab3
Flashman2000 <32598745+Flashman2000@users.noreply.github.com>
2020-10-30 19:46:44 -0400

    Update fastqgen.sh

--- fastqgen.sh
+++ fastqgen.sh
@@ -6,7 +6,7 @@ mkdir /scratch/users/vst14/PB_Test/7069_D/
 i=1
 for d in *7069_D*.bam
 do
-	sbatch --export=file=$d,patient="7069_D",ID=$i genericfastq.slurm
+	sbatch --export=file=$d,patient="7069_D",ID=$i /mnt/rds/txl80/LaframboiseLab/vst14/SLURMS/genericfastq.slurm
 	((i++))
 done
 
@@ -14,7 +14,7 @@ mkdir /scratch/users/vst14/PB_Test/7068_D/
 i=1
 for d in *7068_D*.bam
 do
-	sbatch --export=file=$d,patient="7068_D",ID=$i genericfastq.slurm
+	sbatch --export=file=$d,patient="7068_D",ID=$i /mnt/rds/txl80/LaframboiseLab/vst14/SLURMS/genericfastq.slurm
 	((i++))
 done
 
@@ -22,7 +22,7 @@ mkdir /scratch/users/vst14/PB_Test/7066_D/
 i=1
 for d in *7066_D*.bam
 do
-	sbatch --export=file=$d,patient="7066_D",ID=$i genericfastq.slurm
+	sbatch --export=file=$d,patient="7066_D",ID=$i /mnt/rds/txl80/LaframboiseLab/vst14/SLURMS/genericfastq.slurm
 	((i++))
 done
 
@@ -30,7 +30,7 @@ mkdir /scratch/users/vst14/PB_Test/7055_D/
 i=1
 for d in *7055_D*.bam
 do
-	sbatch --export=file=$d,patient="7055_D",ID=$i genericfastq.slurm
+	sbatch --export=file=$d,patient="7055_D",ID=$i /mnt/rds/txl80/LaframboiseLab/vst14/SLURMS/genericfastq.slurm
 	((i++))
 done
 
@@ -38,7 +38,7 @@ mkdir /scratch/users/vst14/PB_Test/7054_D/
 i=1
 for d in *7054_D*.bam
 do
-	sbatch --export=file=$d,patient="7054_D",ID=$i genericfastq.slurm
+	sbatch --export=file=$d,patient="7054_D",ID=$i /mnt/rds/txl80/LaframboiseLab/vst14/SLURMS/genericfastq.slurm
 	((i++))
 done
 
@@ -46,7 +46,7 @@ mkdir /scratch/users/vst14/PB_Test/7053_D/
 i=1
 for d in *7053_D*.bam
 do
-	sbatch --export=file=$d,patient="7053_D",ID=$i genericfastq.slurm
+	sbatch --export=file=$d,patient="7053_D",ID=$i /mnt/rds/txl80/LaframboiseLab/vst14/SLURMS/genericfastq.slurm
 	((i++))
 done
 
@@ -54,7 +54,7 @@ mkdir /scratch/users/vst14/PB_Test/7052_D/
 i=1
 for d in *7052_D*.bam
 do
-	sbatch --export=file=$d,patient="7052_D",ID=$i genericfastq.slurm
+	sbatch --export=file=$d,patient="7052_D",ID=$i /mnt/rds/txl80/LaframboiseLab/vst14/SLURMS/genericfastq.slurm
 	((i++))
 done
 
@@ -62,7 +62,7 @@ mkdir /scratch/users/vst14/PB_Test/7050_D/
 i=1
 for d in *7050_D*.bam
 do
-	sbatch --export=file=$d,patient="7050_D",ID=$i genericfastq.slurm
+	sbatch --export=file=$d,patient="7050_D",ID=$i /mnt/rds/txl80/LaframboiseLab/vst14/SLURMS/genericfastq.slurm
 	((i++))
 done
 
@@ -70,7 +70,7 @@ mkdir /scratch/users/vst14/PB_Test/7049_D/
 i=1
 for d in *7049_D*.bam
 do
-	sbatch --export=file=$d,patient="7049_D",ID=$i genericfastq.slurm
+	sbatch --export=file=$d,patient="7049_D",ID=$i /mnt/rds/txl80/LaframboiseLab/vst14/SLURMS/genericfastq.slurm
 	((i++))
 done
 
@@ -78,7 +78,7 @@ mkdir /scratch/users/vst14/PB_Test/7044_D/
 i=1
 for d in *7044_D*.bam
 do
-	sbatch --export=file=$d,patient="7044_D",ID=$i genericfastq.slurm
+	sbatch --export=file=$d,patient="7044_D",ID=$i /mnt/rds/txl80/LaframboiseLab/vst14/SLURMS/genericfastq.slurm
 	((i++))
 done
 
@@ -86,6 +86,6 @@ mkdir /scratch/users/vst14/PB_Test/7043_D/
 i=1
 for d in *7043_D*.bam
 do
-	sbatch --export=file=$d,patient="7043_D",ID=$i genericfastq.slurm
+	sbatch --export=file=$d,patient="7043_D",ID=$i /mnt/rds/txl80/LaframboiseLab/vst14/SLURMS/genericfastq.slurm
 	((i++))
 done
